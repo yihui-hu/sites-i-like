6ebed9a7f0e0f7e2e1bda31971757ff748e526c5
Yihui <yyihui.hu@gmail.com>
2022-07-06 02:04:40 +0800

    changed modal box shadow

--- pages/index.js
+++ pages/index.js
@@ -54,9 +54,7 @@ function Home(props) {
                 <h1>Contact</h1>
                 <hr></hr>
                 <h2>
-                  If you want to chat, or have your featured site removed, I'm available
-                  on <a href="https://twitter.com/_yihui">Twitter</a> or
-                  via email at yyihui.hu @ gmail.com.
+                  If you want to chat, or have your featured site removed, I&apos;m available on <a href="https://twitter.com/_yihui">Twitter</a> or via email at yyihui.hu @ gmail.com.
                 </h2>
               </motion.div>
             )}
